99ef7c53ad039a9de8910511949f24dcbc9a5f2c
crise74 <122628836+crise74@users.noreply.github.com>
2023-04-06 12:12:12 -0400

    Update AllTests.java

--- src/test/java/pkgSelenium/AllTests.java
+++ src/test/java/pkgSelenium/AllTests.java
@@ -3,7 +3,7 @@ package pkgSelenium;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
+//This is sample for the build
 @RunWith(Suite.class)
 @SuiteClasses({ DummyTest.class })
 
